f3ecd3b6663c840b28e6a599abbacf9668b68990
yuta <c01140430d@edu.teu.ac.jp>
2019-04-17 04:26:22 +0900

    twitter usersテーブル作成

--- database/migrations/2019_04_17_042128_create_twitter_users_table.php
+++ database/migrations/2019_04_17_042128_create_twitter_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTwitterUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('twitter_users', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->string('token', 100);
+            $table->string('token_secret', 100);
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('twitter_users');
     }
 }
