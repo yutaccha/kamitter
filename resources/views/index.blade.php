311b2a0cb8e0f2c96475540470a2626da92158e9
yuta <c01140430d@edu.teu.ac.jp>
2019-05-11 06:34:25 +0900

    パスの修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,9 +7,9 @@
     <title>{{ config('app.name') }}</title>
 
     <!-- Styles -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('./css/app.css') }}" rel="stylesheet">
     <!-- Scripts -->
-    <script src="{{ mix('js/app.js') }}" defer></script>
+    <script src="{{ mix('./js/app.js') }}" defer></script>
 
 </head>
 <body>
