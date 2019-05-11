beeecf01137987ca89962f995f6e5694d7a113d8
yuta <c01140430d@edu.teu.ac.jp>
2019-05-11 09:59:14 +0900

    set route group

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,18 @@
 /**
  * ツイッター認証でTwitter.comと通信する際のルート
  */
+Route::group(['prefix' => 'kamitter/public'], function () {
+
+
 //認証ページにリダイレクトする
-Route::get('auth/twitter/oauth', 'TwitterAuthController@oauth')->name('twitter.oauth');
+    Route::get('auth/twitter/oauth', 'TwitterAuthController@oauth')->name('twitter.oauth');
 //コールバックの処理
-Route::get('auth/twitter/callback', 'TwitterAuthController@handleProviderCallback')->name('twitter.callback');
+    Route::get('auth/twitter/callback', 'TwitterAuthController@handleProviderCallback')->name('twitter.callback');
 
 
 // 他のルートに該当しない場合indexを返す
-Route::get('/{any?}', function () {
-    return view('index');
-})->where('any', '.+');
+    Route::get('/{any?}', function () {
+        return view('index');
+    })->where('any', '.+');
+
+}));
