41dc125ba397b67da8b9b05a6fc3402b27f11f97
yuta <c01140430d@edu.teu.ac.jp>
2019-05-11 13:17:11 +0900

    Update ApiTwitterUserExist.php

--- app/Rules/ApiTwitterUserExist.php
+++ app/Rules/ApiTwitterUserExist.php
@@ -35,6 +35,7 @@ class ApiTwitterUserExist implements Rule
          */
         $twitter_user_id = session()->get('twitter_id');
         $twitter_user = TwitterUser::where('id', $twitter_user_id)->with('systemManagers')->first();
+        info('twi', [$twitter_user]);
         info('sys', [$twitter_user->system_manager]);
         info('id', [$twitter_user->system_manager['id']]);
         $system_manager_id = $twitter_user->system_manager['id'];
